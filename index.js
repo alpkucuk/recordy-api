756e8e104a81e933b050bba93923db7cc85252a2
Alp Kucuk <kucuk.alp0@gmail.com>
2021-05-11 21:50:46 +0300

    Handle unregistered routes.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const handleError = require('./middleware/handle_error');
 const router = require('./api/index');
+const ApiError = require('./util/api_error');
 
 const app = express();
 
@@ -9,6 +10,11 @@ app.use(express.urlencoded({ extended: true }));
 
 // Register api routes
 router.setRoutes(app);
+
+// Handle unregistered routes
+app.use((req, res, next) => {
+  next(new ApiError(404, 'Requested API endpoint not found.'));
+});
 
 // Error handler
 app.use(handleError);
