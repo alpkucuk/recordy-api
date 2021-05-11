60616f85e11d432034f6a18c7b3632b907b400e1
Alp Kucuk <kucuk.alp0@gmail.com>
2021-05-11 23:41:17 +0300

    Add tests for filter record request validation.

--- api/record/record.test.js
+++ api/record/record.test.js
@@ -1,15 +1,66 @@
-const RecordController = require('./record_controller');
+const httpMocks = require('node-mocks-http');
+const ApiError = require('./../../util/api_error');
+const RecordValidator = require('./record_validator');
+
+// Mock for trace constructor calls for ApiError class.
+jest.mock('./../../util/api_error');
 
 describe('Record API Test', () => {
-  describe('RecordController.filterRecords', () => {
-    it('should have a filterRecords function', () => {
-      expect(typeof RecordController.filterRecords).toBe('function');
+  describe('RecordValidator.validate', () => {
+    beforeEach(() => {
+      // Clear all instances and calls to constructor and all methods:
+      ApiError.mockClear();
     });
-  });
 
-  describe('RecordValidator /POST filter', () => {
-    it('should give validation error', () => {
-      expect(true);
+    it('invalid startDate, should give validation error', () => {
+      const req = httpMocks.createRequest();
+      const next = jest.fn();
+      req.body = {
+        startDate: 'invalid date format',
+        endDate: '2012-01-18',
+        minCount: 0,
+        maxCount: 500
+      };
+      RecordValidator.validate()(req, httpMocks.createResponse(), next);
+      expect(ApiError).toHaveBeenCalledTimes(1);
+    });
+
+    it('invalid minCount, should give validation error', () => {
+      const req = httpMocks.createRequest();
+      const next = jest.fn();
+      req.body = {
+        startDate: '2011-12-05',
+        endDate: '2012-01-18',
+        minCount: 'minCountAsString',
+        maxCount: 500
+      };
+      RecordValidator.validate()(req, httpMocks.createResponse(), next);
+      expect(ApiError).toHaveBeenCalledTimes(1);
+    });
+
+    it('missing endDate parameter, should give validation error', () => {
+      const req = httpMocks.createRequest();
+      const next = jest.fn();
+      req.body = {
+        startDate: '2011-12-05',
+        minCount: '0',
+        maxCount: 500
+      };
+      RecordValidator.validate()(req, httpMocks.createResponse(), next);
+      expect(ApiError).toHaveBeenCalledTimes(1);
+    });
+
+    it('all parameters are valid, should validate successfully', () => {
+      const req = httpMocks.createRequest();
+      const next = jest.fn();
+      req.body = {
+        startDate: '2011-12-05',
+        endDate: '2012-01-18',
+        minCount: 0,
+        maxCount: 500
+      };
+      RecordValidator.validate()(req, httpMocks.createResponse(), next);
+      expect(ApiError).toHaveBeenCalledTimes(0);
     });
   });
 });
